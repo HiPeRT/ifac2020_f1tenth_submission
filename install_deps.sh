c238984d7b5eddd01132c471768e3e61ca220902
Ayoub Raji <rajiayoub93@gmail.com>
2020-07-10 00:52:45 +0200

    deps script

---
+++ install_deps.sh
@@ -0,0 +1,16 @@
+sudo apt-get install libyaml-cpp-dev libeigen3-dev cmake
+sudo apt-get install libopenblas-dev liblapack-dev libarpack2-dev libsuperlu-dev
+#if you have problems installing armadillo, go here -> http://arma.sourceforge.net/download.html
+sudo apt-get install libarmadillo-dev libarmadillo9 libarmadillo9-dbgsym
+sudo apt intall python-pip
+
+sudo pip install casadi
+
+#if you have any problem about cmake see the c++ code chapter in readme here https://github.com/kctess5/range_libc
+git clone https://github.com/kctess5/range_libc
+cd range_libc
+mkdir build
+cd build
+cmake ..
+make
+
