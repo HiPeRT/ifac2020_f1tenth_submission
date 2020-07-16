336e8974d73ebf2ed1c054ff0bff2c53337a6f4d
fgavioli <f.gavioli97@gmail.com>
2020-07-16 18:00:39 +0200

    Fixed typo in install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -2,7 +2,7 @@ sudo apt-get install libyaml-cpp-dev libeigen3-dev cmake
 sudo apt-get install libopenblas-dev liblapack-dev libarpack2-dev libsuperlu-dev
 #if you have problems installing armadillo, go here -> http://arma.sourceforge.net/download.html
 sudo apt-get install libarmadillo-dev libarmadillo9 libarmadillo9-dbgsym
-sudo apt intall python-pip
+sudo apt install python-pip
 
 sudo pip install casadi
 
